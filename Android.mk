0284cb8e9e8bc57bea5b77e0e76e8baecbe6383a
Jeff Hamilton <jham@android.com>
2009-05-04 12:32:23 -0700

    Change the makefile to not include the test code in the provider APK.

--- Android.mk
+++ Android.mk
@@ -3,7 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := user
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+# Only compile source java files in this apk.
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_JAVA_LIBRARIES := ext
 
