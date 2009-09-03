f8a5056d346d41f16e9bca3e98321f3de8012595
Evan Millar <emillar@google.com>
2009-09-03 10:16:55 -0700

    Bump db version, since I forgot to in 23713.
    Change-Id: I6f9f530ea90013ae3f2f8550139ef1dbb4186234

--- src/com/android/providers/contacts/OpenHelper.java
+++ src/com/android/providers/contacts/OpenHelper.java
@@ -61,7 +61,7 @@ import java.util.HashMap;
 /* package */ class OpenHelper extends SQLiteOpenHelper {
     private static final String TAG = "OpenHelper";
 
-    private static final int DATABASE_VERSION = 82;
+    private static final int DATABASE_VERSION = 84;
 
     private static final String DATABASE_NAME = "contacts2.db";
     private static final String DATABASE_PRESENCE = "presence_db";
