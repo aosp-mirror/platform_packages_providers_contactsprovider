3f0445d20c0adf53542c74978d9114ab23486f5a
Dianne Hackborn <hackbod@google.com>
2010-02-24 19:59:48 -0800

    Update to reflect android-common no longer in framework.

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_JAVA_LIBRARIES := ext
 
+LOCAL_STATIC_JAVA_LIBRARIES += android-common
+
 LOCAL_PACKAGE_NAME := ContactsProvider
 LOCAL_CERTIFICATE := shared
 
