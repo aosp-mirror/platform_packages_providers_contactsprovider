d015a8321fb9dcbfa96becb909145dfcce3da608
Bai Tao <michaelbai@google.com>
2010-09-20 15:12:15 +0800

    Fix the failed unit test
    Change-Id: I27a7918608ed854c8ed9108a9d2d4d5fc5b24ec8

--- src/com/android/providers/contacts/ContactsProvider2.java
+++ src/com/android/providers/contacts/ContactsProvider2.java
@@ -1586,8 +1586,8 @@ public class ContactsProvider2 extends SQLiteContentProvider implements OnAccoun
             if (values.containsKey(Phone.NUMBER)) {
                 String number = values.getAsString(Phone.NUMBER);
 
-                String numberE164 = number;
-                    PhoneNumberUtils.formatNumberToE164(number, getCurrentCountryIso());
+                String numberE164 =
+                        PhoneNumberUtils.formatNumberToE164(number, getCurrentCountryIso());
                 if (numberE164 != null) {
                     values.put(PhoneColumns.NORMALIZED_NUMBER, numberE164);
                 }
