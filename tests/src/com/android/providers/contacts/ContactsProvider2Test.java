649a221f80df1c995881863a2b1e31919f7c7eaa
Ricky Wai <rickywai@google.com>
2016-01-21 18:34:53 +0000

    Add test case for ContactsProvider2.rewriteCorpDirectories
    Change-Id: I154430695195c64bd9d13a7d0bbcc1e99c984b25

--- tests/src/com/android/providers/contacts/ContactsProvider2Test.java
+++ tests/src/com/android/providers/contacts/ContactsProvider2Test.java
@@ -2200,6 +2200,46 @@ public class ContactsProvider2Test extends BaseContactsProvider2Test {
         c.close();
     }
 
+    public void testRewriteCorpDirectories() {
+        // 6 columns
+        final MatrixCursor c = new MatrixCursor(new String[] {
+                Directory._ID,
+                Directory.PACKAGE_NAME,
+                Directory.TYPE_RESOURCE_ID,
+                Directory.DISPLAY_NAME,
+                Directory.ACCOUNT_TYPE,
+                Directory.ACCOUNT_NAME,
+        });
+
+        // First, convert and make sure it returns an empty cursor.
+        Cursor rewritten = ContactsProvider2.rewriteCorpDirectories(c);
+
+        assertEquals(0, rewritten.getCount());
+        assertEquals(6, rewritten.getColumnCount());
+
+        c.addRow(new Object[] {
+                5L, // Directory._ID
+                "name", // Directory.PACKAGE_NAME
+                123, // Directory.TYPE_RESOURCE_ID
+                "display", // Directory.DISPLAY_NAME
+                "atype", // Directory.ACCOUNT_TYPE
+                "aname", // Directory.ACCOUNT_NAME
+        });
+
+        rewritten = ContactsProvider2.rewriteCorpDirectories(c);
+        assertEquals(1, rewritten.getCount());
+        assertEquals(6, rewritten.getColumnCount());
+
+        rewritten.moveToPosition(0);
+        int column = 0;
+        assertEquals(1000000005L, rewritten.getLong(column++));
+        assertEquals("name", rewritten.getString(column++));
+        assertEquals(123, rewritten.getInt(column++));
+        assertEquals("display", rewritten.getString(column++));
+        assertEquals("atype", rewritten.getString(column++));
+        assertEquals("aname", rewritten.getString(column++));
+    }
+
     public void testPhoneUpdate() {
         ContentValues values = new ContentValues();
         Uri rawContactUri = mResolver.insert(RawContacts.CONTENT_URI, values);
