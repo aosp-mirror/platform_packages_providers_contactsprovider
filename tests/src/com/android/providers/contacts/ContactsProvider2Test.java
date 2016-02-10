d2f3ed91e988e487ab4213c5c27659097d490c18
Ricky Wai <rickywai@google.com>
2016-02-10 11:11:23 +0000

    Fix failed enterprise unit tests in CP2
    Changes:
    - queryCorpContactsProvider will throw exception if
    it receives non-cp2 query
    - Resolved the conflicts between ag/861964 and ag/861953
    so test cases will check empty cursor not null cursor
    - Remove getCorpUserHandle(c) in tests as no code using it.
    (Proguard will stripped out unused method in CP2 and cause test fails)
    
    Bug: 27114665
    Change-Id: I33e87ada0bc1b62174c9a0d9df8840ba3fa23a3c

--- tests/src/com/android/providers/contacts/ContactsProvider2Test.java
+++ tests/src/com/android/providers/contacts/ContactsProvider2Test.java
@@ -2708,16 +2708,40 @@ public class ContactsProvider2Test extends BaseContactsProvider2Test {
 
         // No corp user.  Primary only.
         assertEquals(-1, UserUtils.getCorpUserId(mActor.getProviderContext()));
-        assertNull(provider.queryCorpContactsProvider(enterpriseUri, null, null, null,
-                null, null));
+        assertEquals(0, provider.queryCorpContactsProvider(enterpriseUri, null, null, null,
+                null, null).getCount());
 
         final SynchronousContactsProvider2 corpCp2 = setUpCorpProvider();
+        // Insert a contact to the corp CP2
+        long rawContactId = ContentUris.parseId(
+                corpCp2.insert(RawContacts.CONTENT_URI, new ContentValues()));
+        // Insert a name
+        ContentValues cv = cv(
+                Data.RAW_CONTACT_ID, rawContactId,
+                Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE,
+                StructuredName.DISPLAY_NAME, "Contact2 Corp",
+                StructuredName.GIVEN_NAME, "Contact2",
+                StructuredName.FAMILY_NAME, "Corp");
+        corpCp2.insert(ContactsContract.Data.CONTENT_URI, cv);
+        // Insert a number
+        cv = cv(
+                Data.RAW_CONTACT_ID, rawContactId,
+                Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE,
+                Phone.NUMBER, "408-222-2222",
+                Phone.TYPE, Phone.TYPE_HOME);
+        corpCp2.insert(ContactsContract.Data.CONTENT_URI, cv);
         // Primary + corp
         um.setUsers(MockUserManager.PRIMARY_USER, MockUserManager.CORP_USER);
-        assertNotNull(provider.queryCorpContactsProvider(enterpriseUri, null, null, null,
-                null, null));
-        assertNull(provider.queryCorpContactsProvider(invalidAuthorityUri, null, null,
-                null, null, null));
+        // It returns 2 identical rows, probably because of the join in phone_lookup.
+        assertEquals(2, provider.queryCorpContactsProvider(enterpriseUri, null, null, null,
+                null, null).getCount());
+        try {
+            provider.queryCorpContactsProvider(invalidAuthorityUri, null, null,
+                    null, null, null);
+            fail(invalidAuthorityUri.toString() + " should throw IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // Expected
+        }
     }
 
     /**
