38e9dedbabfcc39c904cbc02672185b709f079c3
Sandy Roberts <sandyroberts@google.com>
2016-09-14 16:31:28 -0700

    Remove unneeded import ActivityManagerNative
    Change-Id: I8bf4ce8a4fbc1735f23b932a66355ccfe2a1d952

--- src/com/android/providers/contacts/ContactsUpgradeReceiver.java
+++ src/com/android/providers/contacts/ContactsUpgradeReceiver.java
@@ -16,7 +16,6 @@
 
 package com.android.providers.contacts;
 
-import android.app.ActivityManagerNative;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
